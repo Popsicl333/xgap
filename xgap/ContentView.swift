0f4dd3f2e95e60847ee94371f777c98bdc4f6433
Daniel <appledevelopersccs@gmail.com>
2026-02-12 20:18:46 -0500

    Initial Commit

---
+++ xgap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  xgap
+//
+//  Created by Daniel Hong on 2/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ xgap/xgapApp.swift
@@ -0,0 +1,17 @@
+//
+//  xgapApp.swift
+//  xgap
+//
+//  Created by Daniel Hong on 2/12/26.
+//
+
+import SwiftUI
+
+@main
+struct xgapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
